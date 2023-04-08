5e8077385310456c260ae98869f72138b9f62a2e
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-04-08 16:30:06 +0530

    facebook sign in app domain issue resolved

--- src/main/java/com/padmanabhasmac/facebook/auth/facebooksignin/configurations/WebSecurityConfig.java
+++ src/main/java/com/padmanabhasmac/facebook/auth/facebooksignin/configurations/WebSecurityConfig.java
@@ -3,9 +3,12 @@ package com.padmanabhasmac.facebook.auth.facebooksignin.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +16,9 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+//        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+//                .httpBasic(withDefaults());
         return http.build();
     }
-}*/
+}
+*/
--- src/main/java/com/padmanabhasmac/facebook/auth/facebooksignin/controllers/FacebookSignInController.java
+++ src/main/java/com/padmanabhasmac/facebook/auth/facebooksignin/controllers/FacebookSignInController.java
@@ -1,6 +1,7 @@
 package com.padmanabhasmac.facebook.auth.facebooksignin.controllers;
 
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +10,17 @@ import java.security.Principal;
 @RestController
 public class FacebookSignInController {
 
-    @GetMapping("/home")
+    @GetMapping("/auth/facebook")
     public String welcome(Principal principal) {
-        System.out.println("Welcome method init");
-//        Map<String, Object> userDetails = (Map<String, Object>) ((OAuth2AuthenticationToken) principal).getDetails();
-//        String userName = (String) userDetails.get("name");
+        OAuth2User principal1 = ((OAuth2AuthenticationToken) principal).getPrincipal();
+        String name = principal1.getAttribute("name");
+        String email = principal1.getAttribute("email");
+        String profilePicture = principal1.getAttribute("picture");
+        System.out.println("principal = " + principal);
+        System.out.println("principal1 = " + principal1);
+        System.out.println("name: " + name);
+        System.out.println("email: " + email);
+        System.out.println("profilePicture: " + profilePicture);
         return "Hi " + ((OAuth2AuthenticationToken) principal).getPrincipal().getAttribute("name") + " Welcome to my application, logged in with Facebook!!!";
     }
 }
